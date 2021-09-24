ad179469e94c5eeeaca26efd7aaf81f5a019e55d
Fabian Hendriks <19959@ma-web.nl>
2021-09-24 10:17:56 +0200

    update getStedentrips.php

--- getStedentrips.php
+++ getStedentrips.php
@@ -1,1 +1,11 @@
 <?php
+include_once('IdP/IdP.php');
+$username = "reisbureau ZIP";
+$password = "123";
+$credentials = array();
+$credentials['username'] = $username;
+$credentials['password'] = $password;
+$credentials['exp'] = time() + (60*60);
+
+$idp = new IdP($credentials);
+
